d2ac174d73eb2d7159f5a8ab5f45163e7f6e76ea
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-06-27 19:33:27 +0530

    Time: 2553 ms (5%), Space: 55.4 MB (92.72%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] sortArray(int[] arr) {
+        for(int i=1;i<arr.length;i++){
+            int j=i-1;
+            int key=arr[i];
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+                }
+               arr[j+1]=key;
+            }
+            return arr;
+        }
+        
+    
+}
