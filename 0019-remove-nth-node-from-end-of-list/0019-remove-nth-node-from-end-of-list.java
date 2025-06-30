0bfe672bd926c837bd64f498491e60a8487fb94c
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-06-30 13:18:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,32 +10,33 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // ListNode slow=head;
-        // ListNode fast=head;
-        // if(slow.next==null) return null;
-        // for(int i=0;i<n;i++){
-        //    fast=fast.next;
-        // }
-        // while( fast!=null && fast.next!=null){
-        //     slow=slow.next;
-        //     fast=fast.next;
-        // }
-        //    slow.next=slow.next.next;
-        //         return head;
-        ListNode temp=head;
-        int size=0;
-        while(temp!=null){
-            size=size+1;
-            temp=temp.next;
+        ListNode slow=head;
+        ListNode fast=head;
+        if(slow.next==null) return null;
+        if(fast.next==null) head=head.next;
+        for(int i=1;i<=n;i++){
+           fast=fast.next;
         }
-        ListNode t=head;
-        if(size==1 && n==1) return null;
-        if(size==n ) return head.next;
-        for(int i=0;i<size-n-1;i++){
-            t=t.next;
+        while( fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
         }
-        t.next=t.next.next;
-        return head;
+           slow.next=slow.next.next;
+            return head;
+        // ListNode temp=head;
+        // int size=0;
+        // while(temp!=null){
+        //     size=size+1;
+        //     temp=temp.next;
+        // }
+        // ListNode t=head;
+        // if(size==1 && n==1) return null;
+        // if(size==n ) return head.next;
+        // for(int i=0;i<size-n-1;i++){
+        //     t=t.next;
+        // }
+        // t.next=t.next.next;
+        // return head;
     }
    
 }
