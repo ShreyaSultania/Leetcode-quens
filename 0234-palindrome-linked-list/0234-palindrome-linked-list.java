289c4db43b86a2a7ebd2ca40fb48ac1368e4048f
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-07-03 21:58:11 +0530

    Time: 3 ms (99.69%), Space: 69.1 MB (16.41%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,34 +11,57 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         // make a deep copy of the given linked list
-        ListNode dummy = new ListNode(0);
-        ListNode t = dummy;
-        ListNode temp = head;
-        while (temp != null) {
-            ListNode a=new ListNode(temp.val);
-            t.next = a;
-            t = a;
-            temp = temp.next;
-        }
-        ListNode t1 = dummy.next;
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode Next = null;
-        while (curr != null) {
-            Next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = Next;
-        }
+        // ListNode dummy = new ListNode(0);
+        // ListNode t = dummy;
+        // ListNode temp = head;
+        // while (temp != null) {
+        //     ListNode a=new ListNode(temp.val);
+        //     t.next = a;
+        //     t = a;
+        //     temp = temp.next;
+        // }
+        // ListNode t1 = dummy.next;
+        // ListNode prev = null;
+        // ListNode curr = head;
+        // ListNode Next = null;
+        // while (curr != null) {
+        //     Next = curr.next;
+        //     curr.next = prev;
+        //     prev = curr;
+        //     curr = Next;
+        // }
 
-        ListNode t2 = prev;
-        while (t1 != null && t2 != null) {
-            if (t1.val != t2.val)
-                return false;
-            t1 = t1.next;
-            t2 = t2.next;
-        }
+        // ListNode t2 = prev;
+        // while (t1 != null && t2 != null) {
+        //     if (t1.val != t2.val)
+        //         return false;
+        //     t1 = t1.next;
+        //     t2 = t2.next;
+        // }
 
+        // return true;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode prev=null;
+        ListNode Next=null;
+        ListNode curr=slow;
+        while(curr!=null){
+            Next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=Next;
+        }
+        ListNode i=head;
+        ListNode j=prev;
+        while(j!=null){
+            if(i.val!=j.val) return false;
+            i=i.next;
+            j=j.next;
+        }
         return true;
     }
 }
