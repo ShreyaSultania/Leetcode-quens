d98764e7a973c673eec3721fee4fd63a38246ce5
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-07-16 22:12:24 +0530

    Time: 0 ms (100%), Space: 41.1 MB (35.25%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0) return false;
+        while(n>1){
+            int rem=n%4;
+            if(rem!=0) return false;
+            n=n/4;
+        }
+        return true;
+    }
+}
