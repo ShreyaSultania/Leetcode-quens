1044d5f4697d6a3884ae04df4846070a66b8571b
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-07-17 11:30:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int row=grid.length;
+        int cols=grid[0].length;
+        for(int i=1;i<row;i++){
+            grid[0][i]=grid[0][i]+grid[0][i-1];
+        }
+        for(int i=1;i<cols;i++){
+            grid[i][0]=grid[i][0]+grid[i-1][0];
+        }
+        for(int i=1;i<row;i++){
+            for(int j=1;j<cols;j++){
+            grid[i][j]=Math.min(grid[i-1][j],grid[i][j-1]+grid[i][j])
+            }
+        }
+        return grid[row-1][cols-1];
+    }
+}
