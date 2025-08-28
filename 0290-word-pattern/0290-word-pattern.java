fd92f0e0d7e9f1a52731f1af95452ca5893ede36
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-08-29 00:47:09 +0530

    Time: 1 ms (79.43%), Space: 41.5 MB (66.13%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         HashMap<Character,String>map=new HashMap<>();
+        HashMap<String,Character>map1=new HashMap<>();
         String []str=s.split(" ");
+        if(str.length!=pattern.length()) return false;
         for(int i=0;i<pattern.length();i++){
             char ch=pattern.charAt(i);
             if(!map.containsKey(ch)){
@@ -12,7 +14,15 @@ class Solution {
                     return false;
                 }
             }
+            if(!map1.containsKey(str[i])){
+                map1.put(str[i],ch);
+            }
+            else{
+                if(ch!=(map1.get(str[i]))){
+                    return false;
+                }
+            }
         }
-   return true;
+      return true;
     }
 }
