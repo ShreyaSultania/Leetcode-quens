8f5fbc0aeef8b20945dea03499e657b1b5c99c98
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-09-05 16:19:19 +0530

    Time: 0 ms (100%), Space: 40.6 MB (80%) - LeetHub

---
+++ 2749-minimum-operations-to-make-the-integer-zero/2749-minimum-operations-to-make-the-integer-zero.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int makeTheIntegerZero(int num1, int num2) {
+        for(int i=0;i<=60;i++){
+            long val=(long)num1-(long)i*num2;
+            if(val<0) return -1;
+            if(Long.bitCount(val)<=i && i<=val){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
