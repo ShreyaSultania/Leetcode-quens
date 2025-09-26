57e4789a2c9b717eb25cab5ec67b75c38c102913
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-09-26 20:19:28 +0530

    Time: 0 ms (100%), Space: 41.8 MB (96.65%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        String res="";
+        if(nums[0]+nums[1]<=nums[2] || nums[1]+nums[2]<=nums[0] || nums[0]+nums[2]<=nums[1]){
+            return "none";
+        }
+        else if(nums[0]==nums[1] && nums[1]==nums[2] && nums[0]==nums[2]){
+            return "equilateral";
+        }
+        else if(nums[0]==nums[1] || nums[1]==nums[2] || nums[0]==nums[2]){
+            return "isosceles";
+        }
+        else{
+         return "scalene";
+        }
+        
+    }
+}
