3cd3c6f0e20eddfa6a2d387047fca5d0a4d4beb0
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-10-22 12:52:11 +0530

    Time: 3 ms (84.44%), Space: 49.9 MB (40.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -55,11 +55,20 @@ class Solution {
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1' && !visited[i][j]){
-                    bfs(grid,visited,i,j);
+                    // bfs(grid,visited,i,j);
+                       dfs(grid,visited,i,j);
                     c++;
                 }
             }
         }
         return c;
     }
+    void dfs(char[][] grid,boolean [][]visited,int i, int j){
+        visited[i][j]=true;
+        if(i-1>=0 && grid[i-1][j]=='1' && visited[i-1][j]==false)dfs(grid,visited,i-1,j);
+        if(i+1<grid.length && grid[i+1][j]=='1' && visited[i+1][j]==false)dfs(grid,visited,i+1,j);
+        if(j-1>=0 && grid[i][j-1]=='1' && visited[i][j-1]==false)dfs(grid,visited,i,j-1);
+        if(j+1<grid[0].length && grid[i][j+1]=='1' && visited[i][j+1]==false)dfs(grid,visited,i,j+1);
+    }
+
 }
