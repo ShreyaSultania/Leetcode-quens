eb251b3af48f294ffe39d94e0144941985f21eaf
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-10-29 14:46:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,34 @@
 class Solution {
-    public boolean canFinish(int num, int[][] pre) {
-       List<List<Integer>>adj=new ArrayList<>();
-        for(int i=0;i<num;i++){
+    static boolean ans=true;
+    void dfs(int i, List<List<Integer>>adj, boolean visited[], boolean path[]){
+        visited[i]=true;
+        path[i]=true;
+        for(int j=0;j<adj.get(i).size();j++){
+            if(path[adj.get(i).get(j)]==true){
+               ans=false;
+                return;
+            }
+            if(!visited[adj.get(i).get(j)] && !path[adj.get(i).get(j)]){
+                dfs(adj.get(i).get(j),adj,visited,path);
+            }
+        }
+         path[i]=false;
+    }
+    public boolean canFinish(int n, int[][] pre) {
+        List<List<Integer>>adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
             ArrayList<Integer>arr=new ArrayList<>();
             adj.add(arr);
         }
-
-         int in[]=new int[num];
+        
         for(int i=0;i<pre.length;i++){
             int u=pre[i][0];
             int v=pre[i][1];
-            // v->u
             adj.get(v).add(u);
-            in[u]++;
         }
-      
-       ArrayList<Integer>topo=new ArrayList<>();
-       Boolean visited[]=new Boolean[num];
-      Queue<Integer>q=new LinkedList<>();
-      for(int i=0;i<num;i++){
-        if(in[i]==0){
-            q.add(i);
-            visited[i]=true;
-        }
-      }
-      while(!q.isEmpty()){
-        int a=q.remove();
-        topo.add(a);
-        for(int i=0;i<adj.get(a).size();i++){
-            in[adj.get(a).get(i)]--;
-            if(in[adj.get(a).get(i)]==0){
-                q.add(adj.get(a).get(i));
-                visited[adj.get(a).get(i)]=true;
-                
-            }
-        }
-      }
-  if(num==topo.size()) return true;
-  return false;
+     boolean visited[]=new boolean[n];
+     boolean path[]=new boolean[n];
+     dfs(0,adj,visited,path);
+     return ans;
     }
 }
