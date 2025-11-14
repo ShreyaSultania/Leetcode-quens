32a09e5505ed2b92a2dfab5078bca61fd7b83172
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-11-14 12:48:04 +0530

    Time: 0 ms (100%), Space: 43.8 MB (7.29%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        // transpose
+        int m=matrix.length;
+        int n=matrix[0].length;
+        for(int i=0;i<m-1;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+    //    swapping 
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n/2;j++){
+            int temp=matrix[i][j];
+            matrix[i][j]=matrix[i][n-1-j];
+            matrix[i][n-1-j]=temp;
+
+        }
+    }
+    }
+}
