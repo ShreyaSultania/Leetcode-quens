b88a48de4d12aa4643bd9535e5af9937a5581b0b
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-11-15 20:29:33 +0530

    Time: 24 ms (72.55%), Space: 48.9 MB (8.86%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,40 +1,15 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-    //     int c=0;
-    //    for(int i=0;i<nums.length;i++){
-    //     int sum=0;
-    //     for(int j=i;j<nums.length;j++){
-    //        sum=sum+nums[j];
-    //        if(sum==k) {
-    //         c++;
-    //        }
-    //     }
-    //    }
-    //    return c;
-
-    // OPTIMISED CODE
-    int n=nums.length;
-    int arr[]=new int[n];
-    arr[0]=nums[0];
-    for(int i=1;i<n;i++){
-        arr[i]=arr[i-1]+nums[i];
-    }
-    int c=0;
-    HashMap<Integer,Integer>map=new HashMap<>();
-    map.put(0,1);
-    for(int i=0;i<n;i++){
-        if(map.containsKey(arr[i])){
-            int freq=map.get(arr[i]);
-            map.put(arr[i],freq+1);
-        }
-        else map.put(arr[i],1);
-    }
-    for(int i=0;i<arr.length;i++){
-        if(map.containsKey(arr[i]-k)){
-           int freq= map.get(arr[i]-k);
-           c=c+freq;
-        }
-    }
-    return c;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(0,1);
+        int sum=0;
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum-k)) c+=map.get(sum-k);
+           map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        
+        return c;
     }
 }
