777753189a3aa0768a440127e90ff9177354c62b
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-11-23 20:54:53 +0530

    Time: 1 ms (98.86%), Space: 41.3 MB (69.26%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<stones.length;i++){
+            pq.add(stones[i]);
+        }
+        while(pq.size()>1){
+            int a=pq.peek();
+            pq.remove();
+            int b=pq.peek();
+            pq.remove();
+            if(a!=b){
+                pq.add(a-b);
+            }
+        }
+        if(pq.size()==0) return 0;
+        else return pq.peek();
+    }
+}
