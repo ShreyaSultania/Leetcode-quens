ded0933a2f1f55f50444e87e7b448130a8832ca9
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-11-23 20:56:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,19 +1,16 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0;i<stones.length;i++){
-            pq.add(stones[i]);
+       PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+       for(int i=0;i<stones.length;i++){
+        pq.add(stones[i]);
+       }
+       while(pq.size()>1){
+        int a=pq.remove();
+        int b=pq.remove();
+        if(a!=b){
+            pq.add(Math.abs(a-b));
         }
-        while(pq.size()>1){
-            int a=pq.peek();
-            pq.remove();
-            int b=pq.peek();
-            pq.remove();
-            if(a!=b){
-                pq.add(a-b);
-            }
-        }
-        if(pq.size()==0) return 0;
-        else return pq.peek();
+       }
+       return pq.peek();
     }
 }
