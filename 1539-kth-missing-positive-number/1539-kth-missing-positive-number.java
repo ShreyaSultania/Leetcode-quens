623b582ba458d22b7a4b1c53a7bc11d41de8de0c
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-12-14 16:54:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            set.add(arr[i]);
+        }
+        int c=0;
+        for(int i=1;i<=1000;i++){
+            if(!set.contains(i)){
+                c++;
+            }
+            if(c==k) return i;
+        }
+        return -1;
+    }
+}
