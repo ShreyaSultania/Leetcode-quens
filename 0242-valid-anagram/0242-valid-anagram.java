3a3632338288fa2d760fda092a7078895e7e7f9c
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-12-19 13:52:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
         HashMap<Character,Integer>map=new HashMap<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(map.containsKey(ch)){
+            if(!map.containsKey(ch)){
+                map.put(ch,1);
+            }
+            else{
                 int freq=map.get(ch);
                 map.put(ch,freq+1);
             }
-            else{
-                map.put(ch,1);
-            }
         }
         for(int i=0;i<t.length();i++){
-            char ch1=t.charAt(i);
-            if(map.containsKey(ch1)){
-                int freq=map.get(ch1);
-                if(freq==0) return false;
-               else map.put(ch1,freq-1);
+            char ch=t.charAt(i);
+            if(map.containsKey(ch)){
+                int freq=map.get(ch);
+                if(freq==1) map.remove(ch);
+                else map.put(ch,freq-1);
             }
-            else return false;
+           
         }
-        return true;
+        if(map.size()==0) return true;
+        return false;
     }
 }
