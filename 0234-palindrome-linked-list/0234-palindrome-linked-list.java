e138c984c41ea6081ef26d5b4f730f1c8ea0b98d
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2025-12-29 01:19:04 +0530

    Time: 3 ms (99.77%), Space: 94.2 MB (77.65%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,57 +10,27 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        // make a deep copy of the given linked list
-        // ListNode dummy = new ListNode(0);
-        // ListNode t = dummy;
-        // ListNode temp = head;
-        // while (temp != null) {
-        //     ListNode a=new ListNode(temp.val);
-        //     t.next = a;
-        //     t = a;
-        //     temp = temp.next;
-        // }
-        // ListNode t1 = dummy.next;
-        // ListNode prev = null;
-        // ListNode curr = head;
-        // ListNode Next = null;
-        // while (curr != null) {
-        //     Next = curr.next;
-        //     curr.next = prev;
-        //     prev = curr;
-        //     curr = Next;
-        // }
-
-        // ListNode t2 = prev;
-        // while (t1 != null && t2 != null) {
-        //     if (t1.val != t2.val)
-        //         return false;
-        //     t1 = t1.next;
-        //     t2 = t2.next;
-        // }
-
-        // return true;
+        ListNode temp=head;
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
-            slow=slow.next;
             fast=fast.next.next;
+            slow=slow.next;
         }
         ListNode prev=null;
-        ListNode Next=null;
         ListNode curr=slow;
+        ListNode Next=null;
         while(curr!=null){
             Next=curr.next;
             curr.next=prev;
             prev=curr;
             curr=Next;
         }
-        ListNode i=head;
-        ListNode j=prev;
-        while(j!=null){
-            if(i.val!=j.val) return false;
-            i=i.next;
-            j=j.next;
+        ListNode t=prev;
+        while(temp!=null && t!=null){
+            if(temp.val!=t.val) return false;
+           temp=temp.next;
+           t=t.next;
         }
         return true;
     }
