9eef4419f4889e2296a56ad3b205caf66446ab25
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-01-24 10:48:07 +0530

    Time: 34 ms (58.63%), Space: 60.3 MB (14.21%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,25 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       List<List<Integer>>ans=new ArrayList<>();
-       Arrays.sort(nums);
-       int n=nums.length;
-       for(int i=0;i<nums.length-2;i++){
-       if(i!=0 && nums[i]==nums[i-1])  continue;
-        int j=i+1;
-        int k=n-1;
-        while(j<k){
-        int sum=nums[i]+nums[j]+nums[k];
-        if(sum<0) j++;
-        else if(sum>0) k--;
-        else{
-            ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
-            j++;
-            k--;
-         while(j<k && nums[j]==nums[j-1]) j++;
-        while(j<k && nums[k]==nums[k+1]) k--;
+        ArrayList<List<Integer>>ans=new ArrayList<>();
+        Arrays.sort(nums);
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum==0){
+                ArrayList<Integer>temp=new ArrayList<>();
+                temp.add(nums[i]);
+                temp.add(nums[j]);
+                temp.add(nums[k]);
+                ans.add(temp);
+                j++;
+                k--;
+                while(j<k && nums[j]==nums[j-1]) j++;
+                 while(k>j && nums[k]==nums[k+1]) k--;
+            }
+            else if(sum<0){
+                j++;
+            }
+            else k--;
         }
         }
-       }
-       return ans;
+        return ans;
     }
 }
