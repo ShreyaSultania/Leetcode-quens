8d17fe534db821033adc678af2ddaed7f9938de6
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-02-02 12:36:25 +0530

    Time: 64 ms (44.39%), Space: 59.5 MB (10.98%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n=arr.length;
+        // left+1
+        // right+1
+        int []nsi=new int[n];
+        int []psi=new int[n];
+        Stack<Integer>st=new Stack<>();
+        st.push(n-1);
+        nsi[n-1]=n;
+        for(int i=n-2;i>=0;i--){
+            while(!st.isEmpty() && arr[i]<=arr[st.peek()]) st.pop();
+            if(st.isEmpty()) nsi[i]=n;
+            else{
+                nsi[i]=st.peek();
+            }
+            st.push(i);
+        }
+        st.clear();
+        st.push(0);
+        psi[0]=-1;
+        for(int i=1;i<n;i++){
+            while(!st.isEmpty() && arr[i]<arr[st.peek()]) st.pop();
+            if(st.isEmpty()) psi[i]=-1;
+            else{
+                psi[i]=st.peek();
+            }
+            st.push(i);
+        }
+    long sum=0;
+    int mod = 1_000_000_007;
+        for(int i=0;i<n;i++){
+            int a=i-psi[i];
+            int b=nsi[i]-i;
+            sum = (sum + ((long)a * b * arr[i]) % mod) % mod;
+        }
+        return (int)sum;
+    }
+}
