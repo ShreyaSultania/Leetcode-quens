80a199218244249774e6b1bf8899e94ee5b6aa9d
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-02-26 09:43:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,51 +1,53 @@
-class Pair implements Comparable <Pair>{
-    char val;
+class Pair implements Comparable<Pair>{
+    char ch;
     int freq;
-    Pair(char val,int freq){
-        this.val=val;
+    Pair(char ch,int freq){
+        this.ch=ch;
         this.freq=freq;
     }
     public int compareTo(Pair p){
-        return p.freq-this.freq;
+        if(p.freq==this.freq){
+            return this.ch-p.ch;
+        }
+        else{
+            return p.freq-this.freq;
+        }
     }
 }
 class Solution {
     public String reorganizeString(String s) {
-       HashMap<Character,Integer>map=new HashMap<>();
-       for(int i=0;i<s.length();i++){
-        char ch=s.charAt(i);
-        if(!map.containsKey(ch)){
-            map.put(ch,1);
+        PriorityQueue<Pair>pq=new PriorityQueue<>();
+        HashMap<Character,Integer>map=new HashMap<>();
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ele=s.charAt(i);
+            if(!map.containsKey(ele)) map.put(ele,1);
+            else{
+                int freq=map.get(ele);
+                map.put(ele,freq+1);
+            }
         }
-        else{
-            int freq=map.get(ch);
-            map.put(ch,freq+1);
+        for(char ele:map.keySet()){
+            int freq=map.get(ele);
+            pq.add(new Pair(ele,freq));
         }
-       }
-       PriorityQueue<Pair>pq=new PriorityQueue<>();
-       int maxFreq=0;
-       int n=s.length();
-       for(char ele:map.keySet()){
-        int freq=map.get(ele);
-        pq.add(new Pair(ele,freq));
-        maxFreq=Math.max(maxFreq,freq);
-       }
-       if(maxFreq>(n+1)/2) return "";
-       StringBuilder ans=new StringBuilder("");
-       while(pq.size()>1){
-        Pair p=pq.remove();
-        ans.append(p.val);
-        Pair p1=pq.remove();
-        ans.append(p1.val);
-        if(p.freq>1) pq.add(new Pair(p.val,p.freq-1));
-        if(p1.freq>1) pq.add(new Pair(p1.val,p1.freq-1));
-       }
-       if(pq.size()>0){
-        Pair p=pq.remove();
-        ans.append(p.val);
-       }
-       return ans.toString();
-
-
+        String ans="";
+        while(pq.size()>1){
+            Pair p=pq.remove();
+            ans+=p.ch;
+            Pair p1=pq.remove();
+             ans+=p1.ch;
+             if(p.freq>1){
+                pq.add(new Pair(p.ch,p.freq-1));
+            }
+            if(p1.freq>1){
+                pq.add(new Pair(p1.ch,p1.freq-1));
+            }
+        }
+        if(pq.size()==1){
+            Pair p=pq.remove();
+            ans+=p.ch;
+        }
+        return ans;
     }
 }
