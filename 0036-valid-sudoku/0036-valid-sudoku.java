f6fd8374b55dd13ed89a4433ccba92882ce1c404
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-03-03 20:31:10 +0530

    Time: 3 ms (50.99%), Space: 46.7 MB (39.89%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,69 +1,43 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int row=board.length;
-        int col=board[0].length;
-        for(int i=0;i<row;i++){
-            HashMap<Integer,Integer>map=new HashMap<>();
-            for(int j=0;j<col;j++){
-                if(board[i][j]=='.') continue;
-                int ele=board[i][j]-'0';
-                if(!map.containsKey(ele)){
-                    map.put(ele,1);
-                }
-                else{
-                    int freq=map.get(ele);
-                    map.put(ele,freq+1);
+        int row = board.length;
+        int col = board[0].length;
+        for (int i = 0; i < row; i++) {
+            HashSet<Character> set = new HashSet<>();
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] != '.') {
+                    if (set.contains(board[i][j]))
+                        return false;
+                    else
+                        set.add(board[i][j]);
                 }
             }
-            for(int key:map.keySet()){
-            int f=map.get(key);
-            if(key<1 || key>9) return false;
-            if(f>1) return false;
         }
-        }
-
-        // 
-         for(int i=0;i<col;i++){
-             HashMap<Integer,Integer>map1=new HashMap<>();
-            for(int j=0;j<row;j++){
-                if(board[j][i]=='.') continue;
-                int ele=board[j][i]-'0';
-                if(!map1.containsKey(ele)){
-                    map1.put(ele,1);
-                }
-                else{
-                    int freq=map1.get(ele);
-                    map1.put(ele,freq+1);
+        for (int i = 0; i < col; i++) {
+            HashSet<Character> set = new HashSet<>();
+            for (int j = 0; j < row; j++) {
+                if (board[j][i] != '.') {
+                    if (set.contains(board[j][i]))
+                        return false;
+                    else
+                        set.add(board[j][i]);
                 }
             }
-             for(int key:map1.keySet()){
-            int f=map1.get(key);
-            if(key<1 || key>9) return false;
-            if(f>1) return false;
-        }
         }
         for(int i=0;i<row;i+=3){
             for(int j=0;j<col;j+=3){
-                HashMap<Integer,Integer>map2=new HashMap<>();
-                for(int k=0;k<=2;k++){
-                    for(int l=0;l<=2;l++){
-                         if(board[i+k][j+l]=='.') continue;
-                         int ele=board[i+k][j+l]-'0';
-                         if(!map2.containsKey(ele)){
-                    map2.put(ele,1);
-                }
-                else{
-                    int freq=map2.get(ele);
-                    map2.put(ele,freq+1);
-                }
+                HashSet<Character>set=new HashSet<>();
+                for(int k=0;k<3;k++){
+                    for(int l=0;l<3;l++){
+                        if(board[i+k][j+l]!='.'){
+                         if (set.contains(board[i+k][j+l]))
+                            return false;
+                        else
+                            set.add(board[i+k][j+l]);
+                            }
                     }
                 }
-                for(int key:map2.keySet()){
-            int f=map2.get(key);
-            if(key<1 || key>9) return false;
-            if(f>1) return false;
             }
-        }
         }
         return true;
     }
