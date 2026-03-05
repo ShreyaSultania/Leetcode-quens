3a2acef2d6013d3f52a1cd0a17ef734a2c8f75d9
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-03-05 23:12:05 +0530

    Time: 0 ms (100%), Space: 48.2 MB (85.43%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -5,8 +5,8 @@ class Solution {
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) high=mid-1;
-            else low=mid+1;
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
         }
         return -1;
     }
