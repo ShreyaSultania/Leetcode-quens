c93e8be14006dc8c8ac0a54fd11557e7af942794
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-03-05 22:12:01 +0530

    Time: 30 ms (19.38%), Space: 47.4 MB (11.59%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -10,17 +10,18 @@ class Solution {
             char ch=s.charAt(j);
             if(!map.containsKey(ch)){
                 map.put(ch,1);
-                max=1;
             }
             else{
                 int freq=map.get(ch);
                 map.put(ch,freq+1);
-                max=Math.max(max,freq+1);
             }
+            int fre=map.get(ch);
+            max=Math.max(max,fre);
             while(j-i+1-max>k){
                 int freq=map.get(s.charAt(i));
-                map.put(s.charAt(i),freq-1);
-                max=Math.max(max,freq-1);
+                if(freq==1) map.remove(s.charAt(i));
+               else map.put(s.charAt(i),freq-1);
+                // max=Math.max(max,freq-1);
                 i++;
             }
             maxLength=Math.max(maxLength,j-i+1);
