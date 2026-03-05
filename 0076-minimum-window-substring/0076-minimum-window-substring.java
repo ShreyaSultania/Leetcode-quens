4c9acfb9c18601c5c8126d6b375ce0302d4de852
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-03-05 23:04:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,37 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        int n=t.length();
-        int c=t.length();
-        String ans="";
-        int length=Integer.MAX_VALUE;
-        for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            if(!map.containsKey(ch)){
-                map.put(ch,1);
-            }
-            else{
-                int freq=map.get(ch);
-                map.put(ch,freq+1);
-            }
-        }
+        int minLength=Integer.MAX_VALUE;
         int i=0;
         int j=0;
-        while(j<s.length()){
+        int n=s.length();
+        int c=t.length();
+        String ans="";
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int k=0;k<t.length();k++){
+            if(!map.containsKey(t.charAt(k))){
+                map.put(t.charAt(k),1);
+            }
+            else{
+                int freq=map.get(t.charAt(k));
+                map.put(t.charAt(k),freq+1);
+            }
+        }
+        while(j<n){
             char ch=s.charAt(j);
             if(map.containsKey(ch)){
                 int freq=map.get(ch);
-                if(freq>0){
-                    c--;
-                } 
+                if(freq>0) c--;
                 map.put(ch,freq-1);
             }
             while(c==0){
-                if(length>j-i+1){
-                    length=j-i+1;
+                if(minLength>j-i+1){
+                    minLength=j-i+1;
                     ans=s.substring(i,j+1);
                 }
                 if(map.containsKey(s.charAt(i))){
-                int freq=map.get(s.charAt(i));
-                map.put(s.charAt(i),freq+1);
-                if(map.get(s.charAt(i))>0) c++;
+                    int freq=map.get(s.charAt(i));
+                    map.put(s.charAt(i),freq+1);
+                    if(map.get(s.charAt(i))>0) c++;
                 }
                 i++;
             }
