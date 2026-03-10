286d90f6d98ea1cd3b3b598ce4116e5c97d1e7bb
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-03-10 23:28:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,33 +1,21 @@
 class Solution {
-    // void dfs(int i,int j,int[][] adj,boolean [][]visited){
-    //     if(i>=adj.length || j>=adj[0].length) return;
-    //     if(adj[i][j]!=1) return;
-    //     if(adj[i][j]==1) visited[i][j]=true;
-    //     dfs(i+1,j,adj,visited);
-    //     // dfs(i,j+1,adj,visited);
-    // }
-    void dfs(int i,int mat[][],boolean visited[]){
-        int n=mat.length;
-        visited[i]=true;
-        for(int j=0;j<n;j++){
-            if(!visited[j] && mat[i][j]==1){
-                dfs(j,mat,visited);
+    void dfs(int val,int[][] adj,boolean []visited){
+        visited[val]=true;
+        for(int i=0;i<adj[0].length;i++){
+            if(adj[val][i]==1 && !visited[i]){
+                dfs(adj[val][i],adj,visited);
             }
         }
     }
-    public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        // boolean [][]visited=new boolean[n][n];
-        boolean visited[] = new boolean[n];
+    public int findCircleNum(int[][] adj) {
+        int n=adj.length;
         int c=0;
+        boolean []visited=new boolean[n];
         for(int i=0;i<n;i++){
-            // for(int j=0;j<n;j++){
-                // if(isConnected[i][j]==1 && visited[i][j]!=true){
-                if(visited[i]!=true){
-                    dfs(i,isConnected,visited);
-                    c++;
-                }
-            // }
+            if(!visited[i]){
+                dfs(i,adj,visited);
+                c++;
+            }
         }
         return c;
     }
