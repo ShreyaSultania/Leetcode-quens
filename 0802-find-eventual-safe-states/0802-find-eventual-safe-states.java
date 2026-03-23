a876bf9e9b9ffe2fa366eb52d6bf369361ec179c
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-03-23 21:32:56 +0530

    Time: 18 ms (54.98%), Space: 60.2 MB (93.41%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,37 +1,40 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        // reverse the graph
         int n=graph.length;
         List<List<Integer>>adj=new ArrayList<>();
         for(int i=0;i<n;i++){
-            ArrayList<Integer>arr=new ArrayList<>();
-            adj.add(arr);
+            adj.add(new ArrayList<>());
         }
-        int visited[]=new int[n];
+        int []outdegree=new int[n];
         for(int i=0;i<n;i++){
-            for(int j=0;j<graph[i].length;j++){
-                // in original i to j;
-                // reverse j to i
-                adj.get(graph[i][j]).add(i);
-                visited[i]++;
+            outdegree[i]=graph[i].length;
+            for(int ele:graph[i]){
+                adj.get(ele).add(i);
             }
         }
-        ArrayList<Integer>top=new ArrayList<>();
         Queue<Integer>q=new LinkedList<>();
         for(int i=0;i<n;i++){
-           if(visited[i]==0) q.add(i);
+            if(outdegree[i]==0){
+                q.add(i);
+            }
         }
-        while(!q.isEmpty()){
-            int ele=q.remove();
-            top.add(ele);
-            for(int i=0;i<adj.get(ele).size();i++){
-                visited[adj.get(ele).get(i)]--;
-                if(visited[adj.get(ele).get(i)]==0){
-                    q.add(adj.get(ele).get(i));
+        boolean visited[]=new boolean[n];
+        while(q.size()>0){
+            int node=q.remove();
+            visited[node]=true;
+            for(int ele:adj.get(node)){
+                outdegree[ele]--;
+                if(outdegree[ele]==0){
+                    q.add(ele);
                 }
             }
         }
-        Collections.sort(top);
-        return top;
+        List<Integer>ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(visited[i]==true){
+                ans.add(i);
+            }
+        }
+        return ans;
     }
 }
