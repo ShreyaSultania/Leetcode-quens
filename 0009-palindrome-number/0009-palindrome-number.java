d8bf8eb678dc57adf41dfff88d76b1dcc3c7d548
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-04-24 13:52:31 +0530

    Time: 26 ms (5.67%), Space: 45.9 MB (56.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    int reverse(int n){
+        int rev=0;
+        while(n>0){
+            int rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+        }
+        return rev;
+    }
+    public boolean isPalindrome(int x) {
+       int rev= reverse(x);
+       System.out.print(rev);
+       return rev==x;
+    }
+}
