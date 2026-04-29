823bdefec49cc2e255b1d5cc2ac3f18baee3efc9
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-04-29 14:50:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,13 +2,24 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int n=nums.length;
         int idx=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]!=0){
+        //         nums[idx++]=nums[i];
+        //     }
+        // }
+        // for(int i=idx;i<n;i++){
+        //     nums[i]=0;
+        // }/
+
+        //MORE OPTIMAL USING SWAPPING IN ONE ITERATION
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                nums[idx++]=nums[i];
+                int temp=nums[i];
+                nums[i]=nums[idx];
+                nums[idx]=temp;
+                idx++;
             }
         }
-        for(int i=idx;i<n;i++){
-            nums[i]=0;
-        }
+
     }
 }
