5619f0bffcec1398326cf5d12d480d0d833687e3
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-05-04 16:41:17 +0530

    Time: 0 ms (100%), Space: 43.7 MB (69.68%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -4,7 +4,7 @@ class Solution {
         int col=matrix[0].length;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(i>j){
+                if(i<j){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
