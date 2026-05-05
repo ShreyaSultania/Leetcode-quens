492834b97d3018c6183c719fec1df08d4e44f13f
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-05-05 16:43:20 +0530

    Time: 1 ms (99.97%), Space: 77.2 MB (65.94%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // KADANE ALGORITHM IN WHICH WE CAN FIND MAXIMUM SUM OF SUBARRAY
         int n=nums.length;
+        int max=Integer.MIN_VALUE;
         int sum=0;
-        int maxSum=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             sum+=nums[i];
-            if(sum<=0){
-                maxSum=Math.max(maxSum,sum);
-                 sum=0;
+            if(sum<0){
+                max=Math.max(max,sum);
+                sum=0;
             }
-            else maxSum=Math.max(maxSum,sum);
+            else{
+                max=Math.max(max,sum);
+            }
         }
-        return maxSum;
+        return max;
     }
 }
