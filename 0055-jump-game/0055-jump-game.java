b4e705477b5c3f1726b7a59df4209698e0f0845b
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-05-07 23:00:33 +0530

    Time: 379 ms (5.05%), Space: 48 MB (41.35%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,30 @@
 class Solution {
+    boolean helper(int []nums,int idx,int []dp){
+        if(nums.length-1<idx) {
+            return false;
+        }
+        if(nums.length-1==idx){
+            return true;
+        }
+        if(dp[idx]!=-1) return dp[idx]==1;
+        boolean ans=false;
+        for(int i=0;i<nums[idx];i++){
+            if(helper(nums,i+1+idx,dp)){
+                ans=true;
+                return ans;
+        }
+        }
+        if(ans) dp[idx]=1;
+        else dp[idx]=0;
+        return ans;
+    }
+
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        int reached=n-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]+i>=reached){
-                reached=i;
-            }
+        int[]dp=new int[n+1];
+        for(int i=0;i<n+1;i++){
+            dp[i]=-1;
         }
-        if(reached==0) return true;
-        else return false;
+        return helper(nums,0,dp);
     }
 }
