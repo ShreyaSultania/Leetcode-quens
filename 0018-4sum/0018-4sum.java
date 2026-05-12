7949489257f51ddfd51d6dd5ba0bff52fa188acc
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-05-12 11:00:57 +0530

    Time: 18 ms (88.16%), Space: 46.1 MB (21.22%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,28 +1,33 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-       Arrays.sort(nums);
-       List<List<Integer>>ans=new ArrayList<>();
-       int n=nums.length;
-       for(int i=0;i<n-3;i++){
-        if(i>0 && nums[i]==nums[i-1]) continue;
-        for(int j=i+1;j<n-2;j++){
-            if(j>i+1 && nums[j]==nums[j-1]) continue;
-        int l=n-1;
-        int k=j+1;
-        while(k<l ){
-            long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
-            if(sum<target) k++;
-            else if(sum>target) l--;
-            else{
-                ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                k++;
-                l--;
-                while(l>k && nums[k]==nums[k-1]) k++;
-                 while(l>k && nums[l]==nums[l+1]) l--;
+        List<List<Integer>>ans=new ArrayList<>();
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n-1;i++){
+             if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum==target){
+                        List<Integer>temp=new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    }
+                    else if(sum>target) l--;
+                    else k++;
+                }
             }
         }
-               }
-       }
-               return ans;
+        return ans;
     }
 }
