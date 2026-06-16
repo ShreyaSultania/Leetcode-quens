1c31996dc7d4f9d580f94db8e2183f933dc861bf
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-06-16 22:49:43 +0530

    Time: 4 ms (76.47%), Space: 55.4 MB (48.53%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb=new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='#'){
+                sb=sb.append(sb);
+            }
+            else if(ch=='%'){
+                sb=sb.reverse();
+            }
+            else if(ch=='*'){
+                if(sb.length()>0) sb=sb.deleteCharAt(sb.length()-1);
+            }
+            else{
+                sb=sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
