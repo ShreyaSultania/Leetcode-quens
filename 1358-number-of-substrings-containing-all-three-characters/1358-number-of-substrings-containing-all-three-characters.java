c40a45e76cf2ac64f24c66e525c2c5e9ebb4d001
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-06-30 20:46:34 +0530

    Time: 34 ms (17.05%), Space: 47.2 MB (8.17%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,11 +1,11 @@
 class Solution {
-int subarray(String s,int k){
-     int i=0;
-        int j=0;
+    public int numberOfSubstrings(String s) {
         int n=s.length();
-        int c=0;
         HashMap<Character,Integer>map=new HashMap<>();
-        while(j<n){
+        int c=0;
+        int i=0;
+        int j=0;
+            while(j<n){
             char ch=s.charAt(j);
             if(!map.containsKey(ch)){
                 map.put(ch,1);
@@ -14,19 +14,16 @@ int subarray(String s,int k){
                 int freq=map.get(ch);
                 map.put(ch,freq+1);
             }
-            while(map.size()>k){
+            while(map.size()==3){
                 int freq=map.get(s.charAt(i));
                 if(freq==1) map.remove(s.charAt(i));
                 else map.put(s.charAt(i),freq-1);
                 i++;
             }
-            c+=(j-i+1);
-            
+            c+=i;
             j++;
+
         }
         return c;
-}
-    public int numberOfSubstrings(String s) {
-       return subarray(s,3)-subarray(s,2);
     }
 }
