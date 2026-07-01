2644571069d432f2969012795de5633f8ccac463
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-07-01 11:31:53 +0530

    Time: 0 ms (100%), Space: 47.3 MB (94.93%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,15 @@
 class Solution {
+    double helper(double x,long n){
+        if(n==0) return 1.0;
+        double k=helper(x,n/2);
+        if(n%2!=0) return x*k*k;
+        return k*k;
+    }
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if(n<0){
+            long N=n;
+            return 1.0/helper(x,-N);
+        }
+        return helper(x,n);
     }
 }
