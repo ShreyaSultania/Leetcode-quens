c027249ee80a75fd624037e29c775638a0909089
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-07-02 14:01:40 +0530

    Time: 0 ms (100%), Space: 42.3 MB (66.06%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,22 +1,23 @@
 class Solution {
-    List<List<Integer>>ans=new ArrayList<>();
-    void helper(int k,int target,List<Integer>temp,int idx,int sum){
-        if(temp.size()==k){
-            if(sum==target){
+    void helper(int k,int target,List<List<Integer>>ans,List<Integer>temp,int i,int sum){
+        if(target==0 && temp.size()==k){
             ans.add(new ArrayList<>(temp));
             return;
-            }
         }
-        if(sum>target || idx>9 || temp.size()>k) return;
-        helper(k,target,temp,idx+1,sum);//skip
-        temp.add(idx);
-        helper(k,target,temp,idx+1,sum+idx);//take
-        temp.remove(temp.size()-1); //backtrack
+        if(temp.size()>k || target<0 || i>9) return;
+        //take
+        if(target>=i){
+        temp.add(i);
+        helper(k,target-i,ans,temp,i+1,sum);
+        temp.remove(temp.size()-1);
+        }
+        //skip
+        helper(k,target,ans,temp,i+1,sum);
     }
-    public List<List<Integer>> combinationSum3(int k, int target) {
-        List<Integer>temp=new ArrayList<>();
-        helper(k,target,temp,1,0);
-        return ans;
-
+    public List<List<Integer>> combinationSum3(int k, int n) {
+         List<List<Integer>>ans=new ArrayList<>();
+         List<Integer>temp=new ArrayList<>();
+         helper(k,n,ans,temp,1,0);
+         return ans;
     }
 }
