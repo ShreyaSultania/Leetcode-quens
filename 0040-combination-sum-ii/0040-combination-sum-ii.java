81a6dd46ea93447000e0ceb185a8f95825235f6b
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-07-02 13:38:09 +0530

    Time: 6 ms (73.37%), Space: 45.2 MB (67.09%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,22 +1,26 @@
 class Solution {
-    List<List<Integer>>ans=new ArrayList<>();
-    void helper(int []nums,int idx,int target,List<Integer>temp,int sum){
-        if(sum==target){
+    void helper( List<List<Integer>>ans, List<Integer>temp,int []nums,int target,int i){
+        if(target==0){
             ans.add(new ArrayList<>(temp));
             return;
-        }
-        if(idx==nums.length || sum>target) return;
-        int i=idx;
-        while(i+1<nums.length && nums[i]==nums[i+1]) i++;
-        helper(nums,i+1,target,temp,sum);  //skip
-        temp.add(nums[idx]);
-        helper(nums,idx+1,target,temp,sum+nums[idx]);//take
+        } 
+        if(i==nums.length) return;
+        //take
+        if(target>=nums[i]){
+        temp.add(nums[i]);
+        helper(ans,temp,nums,target-nums[i],i+1);
         temp.remove(temp.size()-1);
+        while(i<nums.length-1 && nums[i]==nums[i+1]) i++;
+        }
+        //skip
+        helper(ans,temp,nums,target,i+1);
+
     }
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[]nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>>ans=new ArrayList<>();
         List<Integer>temp=new ArrayList<>();
-        Arrays.sort(candidates);
-        helper(candidates,0,target,temp,0);
+        helper(ans,temp,nums,target,0);
         return ans;
     }
 }
