ebe09e8256ecada8c6fb6fa22d8ee082b96a7433
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-07-11 18:00:54 +0530

    Time: 0 ms (100%), Space: 45.8 MB (22.98%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,14 +14,15 @@
  * }
  */
 class Solution {
-    boolean flag = true;
+    boolean flag=true;
     int helper(TreeNode root){
         if(root==null) return 0;
         int left=1+helper(root.left);
         int right=1+helper(root.right);
         if(Math.abs(left-right)>1){
             flag=false;
-        }
+            return -1;
+        } 
         return Math.max(left,right);
     }
     public boolean isBalanced(TreeNode root) {
