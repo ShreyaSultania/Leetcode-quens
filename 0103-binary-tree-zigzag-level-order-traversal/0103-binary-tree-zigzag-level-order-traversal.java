1eeb9e135ef3d9bd4a248c754b81f1828affabc4
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-07-13 11:51:26 +0530

    Time: 1 ms (80.25%), Space: 43.8 MB (44.49%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,33 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public List<List<Integer>>zigzagLevelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>>ans=new ArrayList<>();
-        Queue<TreeNode>q=new LinkedList<>();
         if(root==null) return ans;
-         q.add(root);
-        while(!q.isEmpty()){
-             int n=q.size();
-             ArrayList<Integer>temp=new ArrayList<>();
-             for(int i=0;i<n;i++){
-            TreeNode curr=q.poll();
-            temp.add(curr.val);
-           if(curr.left!=null) q.add(curr.left);
-             if(curr.right!=null)q.add(curr.right);
-        }
-        ans.add(temp);
-        }
-        for(int i=0;i<ans.size();i++){
-            if(i%2!=0){
-                List<Integer>t=ans.get(i);
-                int k=0;
-                int j=t.size()-1;
-                while(k<=j){
-                    int temp=t.get(k);
-                    t.set(k,t.get(j));
-                    t.set(j,temp);
-                    k++;
-                    j--;
-                }
+        Queue<TreeNode>q=new LinkedList<>();
+        q.add(root);
+        int k=0;
+        while(q.size()>0){
+            int size=q.size();
+            List<Integer>temp=new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode ele=q.remove();
+                temp.add(ele.val);
+                if(ele.left!=null) q.add(ele.left);
+                if(ele.right!=null) q.add(ele.right);
             }
+            if(k%2!=0){
+                Collections.reverse(temp);
+                ans.add(temp);
+            }
+            else ans.add(temp);
+            k++;
         }
         return ans;
     }
