57c497d9f4982f74580dec25af6095f226178045
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-07-15 11:54:29 +0530

    Time: 1 ms (99.98%), Space: 45.6 MB (22.84%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -1,21 +1,36 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    void helper(TreeNode root, int target,List<Integer>temp,List<List<Integer>>res){
+    void helper(List<List<Integer>>ans,List<Integer>temp,TreeNode root,int target,int sum){
         if(root==null) return;
-         target=target-root.val;
-         temp.add(root.val);
-         if(root.left==null && root.right==null && target==0){
-            res.add(new ArrayList<>(temp));
-         }
-          helper(root.left,target,temp,res);
-           helper(root.right,target,temp,res);
-           temp.remove(temp.size()-1);
+        temp.add(root.val);
+        sum+=root.val;
+         if(root.left==null && root.right==null){
+            if(sum==target){
+                ans.add(new ArrayList<>(temp));
+            }
+        }
+        helper(ans,temp,root.left,target,sum);
+        helper(ans,temp,root.right,target,sum);
+       temp.remove(temp.size()-1);
     }
-    public List<List<Integer>> pathSum(TreeNode root, int target) {
-     List<Integer>temp=new ArrayList<>();
-     List<List<Integer>>res=new ArrayList<>();
-     if(root==null) return res;
-     helper(root,target,temp,res);
-     return res;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>temp=new ArrayList<>();
+        helper(ans,temp,root,targetSum,0);
+        return ans;
     }
 }
