15e09e106d77eb7c8c0db691030979be698a8b36
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-07-22 20:21:34 +0530

    Time: 2 ms (99.9%), Space: 44.1 MB (55.59%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -13,50 +13,51 @@
  *     }
  * }
  */
- class Triplet implements Comparable<Triplet>{
-    TreeNode data;
-    int idx;
+class Triplet implements Comparable<Triplet>{
+     TreeNode node;
     int level;
-    Triplet( TreeNode data,int idx, int level){
-        this.data=data;
-        this.idx=idx;
+    int idx;
+    Triplet(TreeNode node,int level,int idx){
+        this.node=node;
         this.level=level;
+        this.idx=idx;
     }
-    public int compareTo(Triplet t){
-        if(t.idx!=this.idx) return this.idx-t.idx;
-       else if(t.level!=this.level) return this.level-t.level;
-        else return this.data.val-t.data.val;
+     public int compareTo(Triplet p){
+        if(p.idx!=this.idx){
+            return this.idx-p.idx;
+        }
+        if(p.level!=this.level){
+            return this.level-p.level;
+        }
+        return this.node.val-p.node.val;
     }
- }
+}
 class Solution {
-    List<List<Integer>>ans;
-    void bfs(TreeNode root){
-        PriorityQueue<Triplet>pq=new PriorityQueue<>();
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>>ans=new ArrayList<>();
         Queue<Triplet>q=new LinkedList<>();
+        PriorityQueue<Triplet>pq=new PriorityQueue<>();
         q.add(new Triplet(root,0,0));
         while(q.size()>0){
-            Triplet t=q.remove();
-            pq.add(t);
-            if(t.data.left!=null){
-                q.add(new Triplet(t.data.left,t.idx-1,t.level+1));
+            Triplet p=q.remove();
+            pq.add(p);
+            if(p.node.left!=null){
+                q.add(new Triplet(p.node.left,p.level+1,p.idx-1));
             }
-            if(t.data.right!=null){
-                q.add(new Triplet(t.data.right,t.idx+1,t.level+1));
+            if(p.node.right!=null){
+                q.add(new Triplet(p.node.right,p.level+1,p.idx+1));
             }
         }
         while(pq.size()>0){
             ArrayList<Integer>temp=new ArrayList<>();
-            int index=pq.peek().idx;
-            while(pq.size()>0 && pq.peek().idx==index){
-                Triplet t=pq.remove();
-                temp.add(t.data.val);
+            Triplet t=pq.remove();
+            temp.add(t.node.val);
+            while(pq.size()>0 && t.idx==pq.peek().idx){
+               Triplet x=pq.remove();
+               temp.add(x.node.val);
             }
             ans.add(temp);
         }
-    }
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        ans=new ArrayList<>();
-        bfs(root);
         return ans;
     }
 }
