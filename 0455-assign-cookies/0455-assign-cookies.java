a643d0ce860bc093556cd198eb1cd20cf70ac74e
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-09-02 22:17:30 +0530

    Time: 14 ms (92.28%), Space: 51.7 MB (6.47%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,15 +1,15 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-       int a=g.length;
-       int b=s.length;
-       Arrays.sort(g);
-       Arrays.sort(s);
-       int i=0;
-       int j=0;
-       while(i<a && j<b){
-        if(s[j]>=g[i]) i++;
-         j++;
-       }
-       return i;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int j=0;
+        int m=g.length;
+        int n=s.length;
+        while(i<m && j<n){
+            if(s[j]>=g[i]) i++;
+            j++;
+        }
+        return i;
     }
 }
