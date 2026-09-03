7094e856d1da3f819643c2ec054159dd2a679062
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-09-03 12:34:20 +0530

    Time: 1 ms (99.9%), Space: 55.5 MB (66.37%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,38 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       HashMap<Integer,Integer>map=new HashMap<>();
+    //    HashMap<Integer,Integer>map=new HashMap<>();
        int n=nums.length;
-       for(int i=0;i<n;i++){
-        if(!map.containsKey(nums[i])){
-            map.put(nums[i],1);
+    //    for(int i=0;i<n;i++){
+    //     if(!map.containsKey(nums[i])){
+    //         map.put(nums[i],1);
+    //     }
+    //     else{
+    //         int freq=map.get(nums[i]);
+    //         map.put(nums[i],freq+1);
+    //     }
+    //    }
+    //    for(int key:map.keySet()){
+    //     int f=map.get(key);
+    //     if(f>(n/2)){
+    //         return key;
+    //     }
+    //    }
+    //    return -1;
+        int lead=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                lead=nums[i];
+                count++;
+            }
+            else if(lead==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-        else{
-            int freq=map.get(nums[i]);
-            map.put(nums[i],freq+1);
-        }
-       }
-       for(int key:map.keySet()){
-        int f=map.get(key);
-        if(f>(n/2)){
-            return key;
-        }
-       }
-       return -1;
+        return lead;
+
     }
 }
