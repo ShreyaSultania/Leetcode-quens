5b8c3aa4795309111bbebd031e82776ed2083b8a
shreya Sultania <152894582+ShreyaSultania@users.noreply.github.com>
2026-09-08 12:35:49 +0530

    Time: 104 ms (5.4%), Space: 42.6 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,23 +1,19 @@
 class Solution {
     int length(int n){
-        int size=0;
-        int temp=n;
-        while(temp!=0){
-            int rem=temp%10;
-            size++;
-            temp=temp/10;
-        }
-        return size;
-    }
-    public int countCommas(int n) {
         int c=0;
-        for(int i=1;i<=n;i++){
-            int l=length(i);
-            if(l>3){
-                c+=(l-1)/3;
-            }
-//   System.out.print(l/3);
+        while(n!=0){
+            c++;
+            n=n/10;
         }
         return c;
+    }
+    public int countCommas(int n) {
+        int commas=0;
+        for(int i=1000;i<=n;i++){
+            
+                commas+=(length(i)-1)/3;
+            
+        }
+        return commas;
     }
 }
